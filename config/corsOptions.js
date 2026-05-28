6011caa03803cf014c32c774facbee2d0d20716e
Manasa <kalavakolanumanasa@gmail.com>
2026-05-28 14:36:41 +0530

    fix: add Vercel URL to CORS allowed origins

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://prepai.yourdomain.com",
+  "https://prepai-frontend-five.vercel.app",
 ];
 
 const corsOptions = {
